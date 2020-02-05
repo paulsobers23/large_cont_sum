3060f348fdf2295869c39521527784172a4cf82e
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-02-05 15:36:14 +0000

    finish

--- large_cont_sum.py
+++ large_cont_sum.py
@@ -1,2 +1,23 @@
-def large_cont_sum():
-    pass
+"""
+Given an array of integers (positive and negative) find the largest continuous sum.
+
+Problem: Find the largest continuous sum
+Example: large_cont_sum([1,2,-1,3,4,10,10,-10,-1]) == 29
+          large_cont_sum([1,2,-1,3,4,-1]) == 9
+          large_cont_sum([-1,1]) == 1
+Algorthim:
+Loop through array 
+Create two var to keep track of sum
+
+
+"""
+
+
+def large_cont_sum(lst):
+    largest_sum = lst[0]
+    for start in range(len(lst)):
+      for stop in range(start,len(lst)):
+        current_sum = sum(lst[start: stop + 1]) 
+        if current_sum > largest_sum:
+          largest_sum = current_sum
+    return largest_sum
--- test_large_cont_sum.py
+++ test_large_cont_sum.py
@@ -1,7 +1,7 @@
 import pytest
 from large_cont_sum import large_cont_sum
 
-@pytest.mark.skip(reason='Implement first')
+# @pytest.mark.skip(reason='Implement first')
 def test_large_cont_sum():
     assert large_cont_sum([1, 2, -1, 3, 4, 10, 10, -10, -1]) == 29
     assert large_cont_sum([1, 2, -1, 3, 4, -1]) == 9
